Using dynamic variable-wise scaling factor and fixed scenario when slce came out empty and was producing an error when computing loglikelihoods
 Changes to be committed:
	modified:   jupyter-notebooks/infer/chain.py
	modified:   jupyter-notebooks/infer/variable.py

jupyter-notebooks/infer/chain.py
@@ -91,17 +91,17 @@ class Chain(object):
         slce = np.argwhere(slce_mask).T[0]
         var.slce = slce
 
+        if slce.shape[0] == 0:
+            return False
+
         Ys_affected = set()
         for i in slce:
             Ys_affected.update(self.map[var.name][i]['Y'])
 
         Y_mask =  np.array(list(Ys_affected))
 
-        try:
-            logratio = loglikelihood[Y_mask].sum() - self.prev_loglikelihood[Y_mask].sum()
-            logratio += var.lgpdf[slce].sum() - var.prev_lgpdf[slce].sum()
-        except IndexError:
-            logratio = - np.inf
+        logratio = loglikelihood[Y_mask].sum() - self.prev_loglikelihood[Y_mask].sum()
+        logratio += var.lgpdf[slce].sum() - var.prev_lgpdf[slce].sum()
 
         accept = logratio >= 0 or logratio > - np.random.exponential()
 
@@ -123,7 +123,7 @@ class Chain(object):
         updt_interval = max(1, N*0.0001)
         steps_until_updt = updt_interval
 
-        tune_interval = updt_interval * 30
+        tune_interval = 100 #updt_interval * 30
         steps_until_tune = tune_interval
         self.acc_rate = 0
         accepted = rejected = 0
@@ -143,10 +143,10 @@ class Chain(object):
             if not steps_until_tune:
                 self.acc_rate = accepted/(accepted + rejected)
 
-                self.tune()
+                #self.tune()
                 for var in self.vars.values():
-                    var.scale = self.scale
-                #    var.tune()
+                #    var.scale = self.scale
+                    var.tune()
 
                 steps_until_tune = tune_interval
                 accepted = rejected = 0

jupyter-notebooks/infer/variable.py
@@ -21,7 +21,7 @@ class Variable(object):
         self.prev_lgpdf = np.zeros(self.size) - np.inf
 
         self.sample_size = max(1, int(self.size*0.05))
-        self.scale = scale
+        self.scale = np.ones(self.size) * scale
         
         self.slce = None
         self.n_proposed = np.zeros(self.size)
@@ -34,11 +34,12 @@ class Variable(object):
 
         slce = np.random.choice(self.size, size=self.sample_size, replace=False)
         prev = self.value[slce]
+        scal = self.scale[slce]
 
-        a, b = (0.01 - prev) / self.scale, (0.99 - prev) / self.scale
-        self.value[slce] = st.truncnorm(a, b, prev, self.scale).rvs()
+        a, b = (0.01 - prev) / scal, (0.99 - prev) / scal
+        self.value[slce] = st.truncnorm(a, b, prev, scal).rvs()
         #self.value[slce] = self.prior.dist(*list(self.args[:, slce])).rvs()
-        self.lgpdf = self.prior.logpdf(self.value)
+        self.lgpdf[slce] = self.prior.dist(*list(self.args[:, slce])).logpdf(self.value[slce])
         
         self.n_proposed[slce] += 1
 
@@ -53,35 +54,35 @@ class Variable(object):
 
     def tune(self):
 
+        candidate_mask = self.n_proposed > 500
         acc_rate = (self.n_proposed - self.n_rejected)/np.maximum(self.n_proposed, 1)
 
-        mask = (acc_rate < 0.001) & (self.n_proposed > 0)
+        mask = (acc_rate < 0.001) & candidate_mask
         # reduce by 90 percent
         self.scale[mask] *= 0.1
 
-        mask = (acc_rate >= 0.001) & (acc_rate <0.05)
+        mask = (acc_rate >= 0.001) & (acc_rate <0.05) & candidate_mask
         # reduce by 50 percent
         self.scale[mask] *= 0.5
 
-        mask = (acc_rate >= 0.050) & (acc_rate <0.20)
+        mask = (acc_rate >= 0.050) & (acc_rate <0.20) & candidate_mask
         # reduce by ten percent
         self.scale[mask] *= 0.9
 
-        mask = (acc_rate >= 0.500) & (acc_rate <0.75)
+        mask = (acc_rate >= 0.500) & (acc_rate <0.75) & candidate_mask
         # increase by ten percent
         self.scale[mask] *= 1.1
 
-        mask = (acc_rate >= 0.750) & (acc_rate <0.95)
+        mask = (acc_rate >= 0.750) & (acc_rate <0.95) & candidate_mask
         # increase by double
         self.scale[mask] *= 2.0
 
-        mask = (acc_rate >= 0.950)
+        mask = (acc_rate >= 0.950) & candidate_mask
         # increase by factor of ten
         self.scale[mask] *= 10.0
 
-        self.scale = np.minimum(self.scale, 0.5)
-        self.scale = np.maximum(self.scale, 0.05)
+        self.scale[candidate_mask] = np.minimum(self.scale[candidate_mask], 0.5)
+        self.scale[candidate_mask] = np.maximum(self.scale[candidate_mask], 0.005)
 
-        self.n_proposed *= 0
-        self.n_rejected *= 0
-
+        self.n_proposed[candidate_mask] *= 0
+        self.n_rejected[candidate_mask] *= 0